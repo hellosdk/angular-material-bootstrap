5d0320b7a8dce094f5a18c93090f018a7bc8bc52
Suresh Dhanakotti <suresh@hellosdk.com>
2025-12-13 20:20:20 -0500

    feat: Initialize a new Angular project with Material and Bootstrap integration, including configuration, components, and styling.

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [MatButtonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+}
